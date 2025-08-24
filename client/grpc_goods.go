cc3d5f12ec837d6094ce32362b2702ee98235782
李正翰 <15504187+Lizh123456@user.noreply.gitee.com>
2025-08-24 20:58:04 +0800

    grpc提交

---
+++ client/grpc_goods.go
@@ -0,0 +1,17 @@
+package client
+
+import (
+	goods "github.com/lizhenghan-cn/goods_proto/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+)
+
+func GoodsGrpc(address string) goods.GoodsClient {
+	conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	return goods.NewGoodsClient(conn)
+
+}
